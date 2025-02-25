cc8cecd4f5364195c886cec8e5eeddb643ea5f68
William Chong <me@williamchong.cloud>
2025-02-25 21:30:07 +0800

    ✨ Support EVM NFT metadata fetching

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -554,9 +554,13 @@ const nuxtConfig = {
       ],
     },
     transpile: [
+      'abitype',
       'gsap',
       'cosmjs-type',
+      'viem',
+      'ox',
       '@cosmjs',
+      '@noble/curves',
       '@likecoin/iscn-js',
       '@likecoin/wallet-connector',
       '@walletconnect',
