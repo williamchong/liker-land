06d0abfd6ce461cb995284435c0a7e02f89a876b
William Chong <me@williamchong.cloud>
2025-04-28 17:16:02 +0800

    🔊 Log store API error if not 404

--- src/pages/reader.vue
+++ src/pages/reader.vue
@@ -141,7 +141,12 @@ export default {
         return;
       }
       await Promise.all([
-        this.lazyFetchNFTBookInfoByClassId(this.classId).catch(() => {}),
+        this.lazyFetchNFTBookInfoByClassId(this.classId).catch((err) => {
+          if (err?.response?.status !== 404) {
+            // eslint-disable-next-line no-console
+            console.error(err);
+          }
+        }),
         this.lazyFetchNFTClassMetadata(),
         this.restoreAuthSession(),
       ]);
